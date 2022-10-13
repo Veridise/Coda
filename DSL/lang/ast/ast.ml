3204735d74d7bb940d527f10261c6d148f7a8dc3
Ian Kretz <44385082+montesquiou@users.noreply.github.com>
2022-10-12 21:17:33 -0500

    Add call statement

--- DSL/lang/ast/ast.ml
+++ DSL/lang/ast/ast.ml
@@ -6,11 +6,45 @@ type expr =
   | Var of string
   | Opp of expr
   | Add of expr * expr
+  | Sub of expr * expr
   | Mul of expr * expr
+
+(* Circuit inputs and outputs *)
+type io =
+  | Expr of expr
+  | List of expr list
+
+type io_opt = io option
 
 (* Statements *)
 type stmt =
   (* Constraint *)
-  | Con of expr * expr
+  | Cons of expr * expr
+  (* Call circuit *)
+  | Call of (io_opt -> stmt list * io_opt) * io_opt * io_opt
 
 type stmts = stmt list
+
+(* Circuits *)
+type circ = io_opt -> stmts * io_opt
+
+(* IsZero *)
+let is_zero (i : io_opt) : stmts * io_opt =
+  match i with
+  | Some (Expr e) ->
+     let stmts = [
+         Cons (Var "out", Add (Opp (Mul (e, Var "inv")), Sig 1)) ;
+         Cons (Mul (e, Var "out"), Sig 0)
+       ] in
+     (stmts, Some (Expr (Var "out")))
+  | _ -> ([], None)
+
+(* IsEqual *)
+let is_equal (i : io_opt) : stmts * io_opt =
+  match i with
+  | Some (List [i0; i1]) ->
+     let stmts = [
+         Call (is_zero, Some (Expr (Sub (i1, i0))), Some (Expr (Var "out")))
+       ] in
+     (stmts, Some (Expr (Var "out")))
+  | _ -> ([], None)
--- DSL/lang/ast/ast.mli
+++ DSL/lang/ast/ast.mli
@@ -3,9 +3,19 @@ type expr =
   | Var of string
   | Opp of expr
   | Add of expr * expr
+  | Sub of expr * expr
   | Mul of expr * expr
 
-type stmt =
-  | Con of expr * expr
+type io =
+  | Expr of expr
+  | List of expr list
+type io_opt = io option
 
+type stmt =
+  | Cons of expr * expr
+  | Call of (io_opt -> stmt list * io_opt) * io_opt * io_opt
 type stmts = stmt list
+
+type circ = io_opt -> stmts * io_opt
+val is_zero: circ
+val is_equal: circ

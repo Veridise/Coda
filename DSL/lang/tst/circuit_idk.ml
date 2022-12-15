ab36e82a8dc62c2722ccfaa1fd0f5fdb987e587d
Ian Kretz <44385082+montesquiou@users.noreply.github.com>
2022-12-15 13:13:20 -0600

    Update after DSL changes

--- DSL/lang/tst/circuit_idk.ml
+++ DSL/lang/tst/circuit_idk.ml
@@ -1,15 +1,17 @@
 (** Circuits for codegen tests *)
 
-open Langast.AST2
+open Lib__Ast
+open Lib__Dsl
 
 let is_zero =
   Circuit {
       name = "IsZero";
-      params = [
+      signals = [
           ("in", Input, tf);
           ("out", Output, tf);
           ("inv", Exists, tf)
         ];
+      property = None;
       body = [
           assert_eq (v "out") (sub (fc 1) (mul (v "in") (v "inv")));
           assert_eq (mul (v "in") (v "out")) (fc 0)
@@ -19,15 +21,17 @@ let is_zero =
 let is_equal =
   Circuit {
       name = "IsEqual";
-      params = [
+      signals = [
           (* Note: Unsure `QP` is correct for `qual` field *)
           ("in", Input, TArr (tf, QP, Const (CInt 2)));
           ("out", Output, tf)
         ];
+      property = None;
       body = [
           slet
             "isz"
-            (Call ("IsZero", [sub (Get (Var "in", Const (CInt 1))) (Get (Var "in", Const (CInt 0)))]));
+            (Call ("IsZero",
+                   [sub (ArrayOp (Get, Var "in", Const (CInt 1))) (ArrayOp (Get, Var "in", Const (CInt 0)))]));
           assert_eq (v "isz") (v "out")
         ]
     }

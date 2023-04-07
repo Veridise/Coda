e80b901b0b5790c0d77733d82a5447249bab16f7
Junrui Liu <junrui@ucsb.edu>
2023-04-07 13:27:52 -0700

    Add default value for F*F list

--- BigInt/src/Default.v
+++ BigInt/src/Default.v
@@ -4,3 +4,4 @@ Require Import Circom.Circom.
 Class Default (T: Type) := { default: T }.
 
 #[global] Instance F_default: Default F := { default := 0 }.
+#[global] Instance F_F_default: Default (F*F) := { default := (0%F,0%F) }.

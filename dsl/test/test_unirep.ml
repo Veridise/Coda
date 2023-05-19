ed273ef88904eeebfe5a3d0a62fdd15d3d7cf4fc
Hanzhi Liu <misaka@sjtu.edu.cn>
2023-05-19 21:48:15 +0800

    wrap up

--- dsl/test/test_unirep.ml
+++ dsl/test/test_unirep.ml
@@ -2,7 +2,7 @@ open Core
 open Typecheck
 open Coqgen
 open Circomlib
-open Unirep.LeafHasher
+open Unirep.CoreCircuits
 open Unirep.IncrementalMerkleTree
 module U = Test_utils.Utils
 
@@ -14,21 +14,21 @@ module U = Test_utils.Utils
 
 (* let _ = U.test identity_secret Circomlib.Poseidon.[poseidon] *)
 
-(* let _ = U.test identity_commitment (Circomlib.Poseidon.[poseidon] @ Unirep.LeafHasher.[identity_secret]) *)
+(* let _ = U.test identity_commitment (Circomlib.Poseidon.[poseidon] @ Unirep.CoreCircuits.[identity_secret]) *)
 
 (* let _ =
    U.test signup
      ( Circomlib.Poseidon.[poseidon]
-     @ Unirep.LeafHasher.[identity_secret1; identity_commitment; state_tree_leaf]
+     @ Unirep.CoreCircuits.[identity_secret1; identity_commitment; state_tree_leaf]
      ) *)
 
-(* let _ = U.test epoch_key_lite (Circomlib.Poseidon.[poseidon] @ Unirep.LeafHasher.[identity_secret1; identity_commitment; state_tree_leaf] @ Circomlib.Bitify.[num2bits]
-   @ Circomlib.Comparators.[less_than] @ Unirep.LeafHasher.[epoch_key_hasher]) *)
+(* let _ = U.test epoch_key_lite (Circomlib.Poseidon.[poseidon] @ Unirep.CoreCircuits.[identity_secret1; identity_commitment; state_tree_leaf] @ Circomlib.Bitify.[num2bits]
+   @ Circomlib.Comparators.[less_than] @ Unirep.CoreCircuits.[epoch_key_hasher]) *)
 
 (* let _ =
    U.test epoch_key
      ( Circomlib.Poseidon.[poseidon]
-     @ Unirep.LeafHasher.
+     @ Unirep.CoreCircuits.
          [identity_secret1; identity_commitment; state_tree_leaf; epoch_key_lite; epoch_key_hasher]
      @ Circomlib.Bitify.[num2bits]
      @ Circomlib.Comparators.[less_than]
@@ -37,7 +37,7 @@ module U = Test_utils.Utils
 (* let _ =
    U.test prevent_double_action
      ( Circomlib.Poseidon.[poseidon]
-     @ Unirep.LeafHasher.
+     @ Unirep.CoreCircuits.
          [ identity_secret1
          ; identity_commitment
          ; state_tree_leaf
@@ -50,7 +50,7 @@ module U = Test_utils.Utils
 (* let _ =
    U.test prove_reputation
      ( Circomlib.Poseidon.[poseidon]
-     @ Unirep.LeafHasher.
+     @ Unirep.CoreCircuits.
          [ identity_secret1
          ; identity_commitment
          ; state_tree_leaf
@@ -63,10 +63,10 @@ module U = Test_utils.Utils
      @ Circomlib.Gates.[cor]
      @ Unirep.IncrementalMerkleTree.[mrkl_tree_incl_pf] ) *)
 
-let _ =
+(* let _ =
   U.test user_state_transition
     ( Circomlib.Poseidon.[poseidon]
-    @ Unirep.LeafHasher.
+    @ Unirep.CoreCircuits.
         [ identity_secret1
         ; identity_commitment
         ; state_tree_leaf
@@ -77,19 +77,19 @@ let _ =
     @ Circomlib.Bitify.[num2bits]
     @ Circomlib.Comparators.[less_than; geq; greater_than; is_zero; is_equal]
     @ Circomlib.Gates.[cor]
-    @ Unirep.IncrementalMerkleTree.[mrkl_tree_incl_pf] )
+    @ Unirep.IncrementalMerkleTree.[mrkl_tree_incl_pf] ) *)
 
 (* let _ =
    U.test upper_less_than
      ( Circomlib.Bitify.[bits2num; num2bits]
      @ Circomlib.Comparators.[less_than]
-     @ Unirep.LeafHasher.[alias_check] ) *)
+     @ Unirep.CoreCircuits.[alias_check] ) *)
 
 (* let _ =
    U.test repl_field_equal
      ( Circomlib.Bitify.[bits2num; num2bits]
      @ Circomlib.Comparators.[is_equal]
-     @ Unirep.LeafHasher.[alias_check] ) *)
+     @ Unirep.CoreCircuits.[alias_check] ) *)
 
 (* let _ =
    U.test mrkl_tree_incl_pf Circomlib.(Poseidon.[poseidon] @ Mux1.[multi_mux_1] @ Comparators.[is_zero]) *)
